0f2be9982451eaeef960c80216586b57044ee621
Felipe-LasHeras <fe.lasheras@duocuc.cl>
2023-11-02 21:34:50 -0300

    Primer commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
+import "../../styles/index.css"
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [color, setColor] = useState(null);
+	const [colorPrevio, setColorPrev] = useState(null)
+
+	const lightColors = {
+		red: color === "red" ? "red" : color === "purple" && colorPrevio == "red" ? "purple" : "gray",
+		yellow: color === "yellow" ? "yellow" : color === "purple" && colorPrevio == "yellow" ? "purple" : "gray",
+		green: color === "green" ? "green" : color === "purple" && colorPrevio == "green" ? "purple" : "gray"
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="semaforo">
+				<div className="luz" style={{backgroundColor: lightColors.red}} onClick={() => setColor("red")}>
+
+				</div>
+				<div className="luz" style={{backgroundColor: lightColors.yellow}} onClick={() => setColor("yellow")}>
+
+				</div>
+				<div className="luz" style={{backgroundColor: lightColors.green}} onClick={() => setColor("green")}>
+
+				</div>
+
+			</div>
+			<button onClick={()=>{
+				const newColor = color === "red" ? "yellow" : color === "yellow" ? "green" : "red"
+				setColor(newColor)
+			}}>Alternar Color</button>
+			<button onClick={()=>{
+				setColorPrev(color)
+				setColor("purple")
+			}}>
+				Purpura
+			</button>
+		</>
 	);
 };
 
